0cdbd8ddb598bb03e47a97c1243b5e95f54ccbed
Mehmet Musabeyoglu <mehmetmusabeyoglu@gmail.com>
2022-10-27 18:15:42 -0700

    Added routes.

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const routerHtml = require('./routes/htmlRoutes');
+const routerApi = require('./routes/apiRoutes');
+
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
+app.use('/', routerHtml);
+app.use('/', routerApi);
 
 app.listen(PORT, () => console.log(`Node is listening and available at http://localhost${PORT}/`));
